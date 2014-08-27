960e9b9e92f12400dd91c1e529c3b4e3bb8fdd9f
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-08-27 10:50:01 +0200

    Fixes to get it

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -10,7 +10,27 @@ module CatalogHelper
   end
 
   def render_format_field_index args
- 
     t "new_subtypes.#{args[:document]['format']}"
+  end
+
+  def get_backlink_origin link
+    case link
+    when /^https?:\/\/orbit.dtu.dk/
+      'orbit'
+    when /^https?:\/\/pure.au.dk/
+      'rdb_au'
+    when /^https?:\/\/curis.ku.dk/
+      'rdb_ku'
+    when /^https?:\/\/pure.itu.dk/
+      'rdb_itu'
+    when /^https?:\/\/research.cbs.dk/
+      'rdb_cbs'
+    when /^https?:\/\/vbn.aau.dk/
+      'rdb_vbn'
+    when /^https?:\/\/rucforsk.ruc.dk/
+      'rdb_ruc'
+    when /^https?:\/\/findresearcher.sdu.dk/
+      'rdb_sdu'
+    end
   end
 end
