7984a36f50e35985bd2529d03bbf4197f0137719
Dagmara <dplo@dhcp163.cvt.dk>
2014-09-03 09:28:28 +0200

    layout modified

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -10,48 +10,13 @@ module CatalogHelper
   end
 
   def render_source_field args
-    case t "source_labels.#{args[:document]['source_ss']}"
-   when /["Orbit"]/
-    'Technical University of Denmark'
-   when /["Rdb Ku"]/
-    'University of Copenhagen'
-   when /["Rdb Itu"]/
-    'IT University of Copenhagen'
-   when /["Rdb Au"]/
-    'Aarhus University'
-   when /["Rdb Cbs"]/
-    'Copenhagen Business School'
-   when /["Rdb Ruc"]/
-    'Roskilde University'
-   when /["Rdb Sdu"]/
-    'University of Southern Denmark'
-   when /["Rdb Vbn"]/
-    'Aalborg University'
-   end
+   case t "source_labels.#{args[:document]['source_ss']}"
+    when /Orbit/
+      'Technical University of Denmark'
+    end
   end
 
   def render_format_field_index args
     t "new_subtypes.#{args[:document]['format']}"
-  end
-
-  def get_backlink_origin link
-    case link
-    when /^https?:\/\/orbit.dtu.dk/
-      'orbit'
-    when /^https?:\/\/pure.au.dk/
-      'rdb_au'
-    when /^https?:\/\/curis.ku.dk/
-      'rdb_ku'
-    when /^https?:\/\/pure.itu.dk/
-      'rdb_itu'
-    when /^https?:\/\/research.cbs.dk/
-      'rdb_cbs'
-    when /^https?:\/\/vbn.aau.dk/
-      'rdb_vbn'
-    when /^https?:\/\/rucforsk.ruc.dk/
-      'rdb_ruc'
-    when /^https?:\/\/findresearcher.sdu.dk/
-      'rdb_sdu'
-    end
   end
 end
