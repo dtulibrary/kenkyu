ad5d940ad24cb7764b1ea72e5db1ba9333904281
Dagmara <dplo@dhcp163.cvt.dk>
2015-03-04 16:22:47 +0100

    Name for 'Research Area' changed in both english, danish; the message in english for empty search result- updated

--- app/assets/javascripts/dropdown_form.js
+++
@@ -1,7 +0,0 @@
-// allows form elements inside a bootstrap dropdown recieve input
-// without this, the dropdown would close when clicking the form elements
-(function($){
-  $('.dropdown.form form input,label').on('click', function (e) {
-    e.stopPropagation();
-  });
-})(jQuery);
